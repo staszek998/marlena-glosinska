09ccd1e9a69f4e560807e9148cf36d3d8135d242
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-02-21 14:39:33 +0100

    First footer edits

--- js/components/footer.jsx
+++ js/components/footer.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter, Route, Link, Switch, NavLink } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Link, Switch, NavLink } from "react-router-dom";
 
 export default class Footer extends React.Component {
-    constructor(props){
-        super(props);
-    };
-    render(){
-        return <footer>footer</footer> ;
-    };
-};
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <footer>
+        <div className="footer__copyright">
+          <span>Design by Stanislaw Gregor &copy; All rights reserved</span>
+        </div>
+        <div className="footer__logo">
+          <span>MG</span>
+        </div>
+        <div className="footer__icons">
+          <div className="footer__icon">
+            <i className="fab fa-facebook-f" />
+          </div>
+          <div className="footer__icon">
+            <i className="fab fa-youtube" />
+          </div>
+          <div className="footer__icon">
+            <i className="fab fa-instagram" />
+          </div>
+          <div className="footer__icon">
+            <i className="fab fa-linkedin-in" />
+          </div>
+          <div className="footer__icon">
+            <i className="fab fa-snapchat-ghost" />
+          </div>
+          <div className="footer__icon">
+            <i className="fab fa-twitter" />
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
