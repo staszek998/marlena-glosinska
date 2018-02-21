915f14b80b6217d29f3c35a61a20560e9e4af9e5
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-02-21 13:52:34 +0100

    First edits of Newsletter

--- js/components/home/newsletter.jsx
+++ js/components/home/newsletter.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter, Route, Link, Switch, NavLink } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Link, Switch, NavLink } from "react-router-dom";
 
 export default class Newsletter extends React.Component {
-    constructor(props){
-        super(props);
-    };
-    render(){
-        return <section className='newsletter' >
-            newsletter
-        </section> ;
-    };
-};
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <section className="newsletter">
+        <div className="newsletter__cols">
+          <div className="newsletter_col" />
+          <form action="submit">
+            <h1>LET'S STAY IN TOUCH</h1>
+            <select name="title" id="title">
+              <option value="" disabled selected>
+                Title
+              </option>
+              <option value="Mr.">Mr.</option>
+              <option value="Mrs.">Mrs.</option>
+            </select>
+            <input type="text" name="first-name" placeholder="First name" />
+            <input type="text" name="last-name" placeholder="Last name" />
+            <input type="email" name="email" placeholder="E-mail" />
+            <input type="submit" value="SIGN UP" />
+          </form>
+          <div className="newsletter_col" />
+        </div>
+      </section>
+    );
+  }
+}
