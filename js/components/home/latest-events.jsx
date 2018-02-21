674a9a7d22b28160373bbd2181a00a7c47c5e61f
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-02-21 11:32:46 +0100

    First edits of the Latest Events section

--- js/components/home/latest-events.jsx
+++ js/components/home/latest-events.jsx
@@ -2,6 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter, Route, Link, Switch, NavLink } from "react-router-dom";
 
+class Event extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="latest-events__event">
+        <span>{this.props.date.toUpperCase()}</span>
+        <span>{this.props.name.toUpperCase()}</span>
+        <span>{this.props.venue.toUpperCase()}</span>
+        <p>{this.props.description}</p>
+        <button>LEARN MORE</button>
+      </div>
+    );
+  }
+}
+
 export default class LatestEvents extends React.Component {
   constructor(props) {
     super(props);
@@ -10,18 +27,37 @@ export default class LatestEvents extends React.Component {
     return (
       <section className="latest-events">
         <div className="latest-events__col">
-          <div className="latest-events__event">
-            <span>DATE</span>
-            <span>VENUE</span>
-            <span>NAME</span>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique sapiente dignissimos natus doloremque quaerat. Perferendis voluptate at dolores assumenda laboriosam non maiores, adipisci, veritatis dolorum beatae sit quas incidunt nisi?</p>
-          </div>
+          <Event
+            date="date"
+            name="name"
+            venue="venue"
+            description="[DESCRIPTION] Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+              sapiente dignissimos natus doloremque quaerat. Perferendis
+              voluptate at dolores assumenda laboriosam non maiores, adipisci,
+              veritatis dolorum beatae sit quas incidunt nisi?"
+          />
         </div>
         <div className="latest-events__col">
-          <div className="latest-events__event">event</div>
+          <Event
+            date="12.12.2012"
+            name="apocalypse"
+            venue="stodoła, warsaw (pl)"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+              sapiente dignissimos natus doloremque quaerat. Perferendis
+              voluptate at dolores assumenda laboriosam non maiores, adipisci,
+              veritatis dolorum beatae sit quas incidunt nisi?"
+          />
         </div>
         <div className="latest-events__col">
-          <div className="latest-events__event">event</div>
+          <Event
+            date="09.10.2018"
+            name="stan's birthday"
+            venue="promenada, warsaw (pl)"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+              sapiente dignissimos natus doloremque quaerat. Perferendis
+              voluptate at dolores assumenda laboriosam non maiores, adipisci,
+              veritatis dolorum beatae sit quas incidunt nisi?"
+          />
         </div>
       </section>
     );
