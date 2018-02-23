15123d6d89f2fbcb5375fd710e8aabbcb1e052c3
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2018-02-23 13:15:29 +0100

    Add media queries for New Music and Listen sections

--- js/components/home/listen.jsx
+++ js/components/home/listen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter, Route, Link, Switch, NavLink } from "react-router-dom";
+import MediaQuery from "react-responsive";
 
 export default class Listen extends React.Component {
   constructor(props) {
@@ -10,13 +11,25 @@ export default class Listen extends React.Component {
     return (
       <section className="listen">
         <h1 className="section-header">IN CASE YOU'RE HERE JUST TO LISTEN</h1>
-        <iframe
-          src="https://open.spotify.com/embed/user/11164081692/playlist/6dqFiHECXIyGXBAVUvaB1L"
-          width="523"
-          height="360"
-          frameBorder="0"
-          allowTransparency="true"
-        />
+        <MediaQuery query="(max-width: 1224px)">
+          <iframe
+            src="https://open.spotify.com/embed?uri=spotify:user:11164081692:playlist:6dqFiHECXIyGXBAVUvaB1L"
+            width="600"
+            height="80"
+            frameBorder="0"
+            allowTransparency="true"
+            allow="encrypted-media"
+          />
+        </MediaQuery>
+        <MediaQuery query="(min-device-width: 1224px)">
+          <iframe
+            src="https://open.spotify.com/embed/user/11164081692/playlist/6dqFiHECXIyGXBAVUvaB1L"
+            width="523"
+            height="360"
+            frameBorder="0"
+            allowTransparency="true"
+          />
+        </MediaQuery>
       </section>
     );
   }
